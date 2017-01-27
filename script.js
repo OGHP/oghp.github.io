27d4aa6bd58d99ec8fb5449dcd38ed412885c89a
heatherpalmer78 <heatherpa@zillow.com>
2017-01-27 15:07:20 -0800

    Working with gallery layouts

---
+++ script.js
@@ -0,0 +1,35 @@
+// Sean's code for sound effect - will need to learn how to code this properly.
+
+// (function() {
+
+//   var muted = false;
+//   var sounds = [];
+//   for(var i = 1; i < 19; i++) {
+//     var sound = new Audio('hits/' + i + '.ogg');
+//     sounds.push(sound);
+//   }
+
+//   function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
+
+// I want my sound to be onmouse over images on home page
+
+//   window.onmouseover = function(e) {
+//     if(muted) {
+//       return;
+//     }
+
+//     sounds[getRandomInt(0, sounds.length - 1)].play();
+//   }
+
+//   document.getElementById('mute-btn').addEventListener('click', function(e) {
+//     muted = !muted;
+//     if(muted) {
+//       e.target.style.backgroundColor = 'red';
+//     } else {
+//       e.target.style.backgroundColor = '';
+//     }
+//   });
+
+// })()
